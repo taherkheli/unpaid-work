e2eee2b9954ef7871debfd4637b0f33c3b9b35ef
bilal taherkheli <bilal76@gmail.com>
2020-10-23 16:55:04 +0200

    Added a simple consumer (a console app) which can consume a resource from the producer API

---
+++ Tax/Consumer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Consumer
+{
+  class Program
+  {
+    static void Main()
+    {
+			Console.WriteLine("Intializing consumer ....");
+			var result = Consume().GetAwaiter().GetResult();
+			Console.WriteLine(result);
+			Console.ReadKey();
+		}
+
+		private static async Task<String> Consume()
+		{
+			var url = "https://localhost:5001/";
+
+			var client = new HttpClient()
+			{
+				BaseAddress = new Uri(url),
+				Timeout = TimeSpan.FromSeconds(30)
+			};
+			client.DefaultRequestHeaders.Clear();
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+			Console.WriteLine("Requesting consumer ....");
+			var response = await client.GetAsync("/api/kommunes");
+			response.EnsureSuccessStatusCode();
+			var content = await response.Content.ReadAsStringAsync();
+
+			return content;
+		}
+
+	}
+}
